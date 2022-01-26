7c519c275f629ec51edbfb688409e4f549a37fb2
Elias Martins Guerra Prado <eliasmgprado@hotmail.com>
2022-01-25 21:18:05 -0300

    up

--- Project8/src/store/index.js
+++ Project8/src/store/index.js
@@ -1,26 +1,16 @@
-import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
-const initalState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter + action.amount;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
   },
 });
+
+export default store;
 
 // const counterReducer = (state = initalState, action) => {
 //   if (action.type === "increment") {
@@ -55,10 +45,3 @@ const counterSlice = createSlice({
 // };
 
 // const store = createStore(counterSlice.reducer);
-const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer,
-  },
-});
-
-export default store;
