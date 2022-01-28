1b54f80145e122ee69624c64fee8dc831982bb51
Elias Martins Guerra Prado <eliasmgprado@hotmail.com>
2022-01-28 07:31:52 -0300

    up

---
+++ Project11/src/components/layout/MainNavigation.js
@@ -0,0 +1,14 @@
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Great Quotes</div>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
